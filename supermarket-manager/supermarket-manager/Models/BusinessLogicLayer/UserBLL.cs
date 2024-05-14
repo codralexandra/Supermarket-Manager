c9e30db87fe2081dd461f50343cedee4bb89bb7d
alex <147138151+codralexandra@users.noreply.github.com>
2024-05-14 22:53:58 +0300

    Implemented functionality to prevent adding an existing user & verifications on fields validity

--- supermarket-manager/supermarket-manager/Models/BusinessLogicLayer/UserBLL.cs
+++ supermarket-manager/supermarket-manager/Models/BusinessLogicLayer/UserBLL.cs
@@ -11,6 +11,34 @@ namespace supermarket_manager.Models.BusinessLogicLayer
         UserDAL userDAL = new UserDAL();
         public ObservableCollection<User> UsersList { get; set; }
 
+        #region helpers
+        private Role GetOpposite(Role role)
+        {
+            switch (role)
+            {
+                case Role.Admin:
+                    return Role.Cashier;
+                case Role.Cashier:
+                    return Role.Admin;
+                default:
+                    return Role.None;
+            }
+        }
+
+        private Role stringToRole(string role)
+        {
+            switch (role)
+            {
+                case "Admin":
+                    return Role.Admin;
+                case "Cashier":
+                    return Role.Cashier;
+                default: return Role.None;
+            }
+        }
+
+        #endregion
+
         public Role GetUserByLogin(string username, string password)
         {
             if(username == null || password == null)
@@ -27,8 +55,23 @@ namespace supermarket_manager.Models.BusinessLogicLayer
 
         public void AddUser(User user)
         {
-            userDAL.AddUser(user);
-            UsersList.Add(user);
+            if(user == null || user.Username=="" || user.Password == "" || user.Role == "")
+            {
+                MessageBox.Show("All fields are mandatory.");
+            }
+            else if(userDAL.GetUserByLogin(user.Username, user.Password)!=Role.None)
+            {
+                MessageBox.Show("User already exists.");
+            }
+            else if(userDAL.GetUserByLogin(user.Username, user.Password)==GetOpposite(stringToRole(user.Role)))
+            {
+                MessageBox.Show("Can't add the same user with a different role."); //Not really neccessary (?)
+            }
+            else
+            {
+                userDAL.AddUser(user);
+                UsersList.Add(user);
+            }
         }
 
         public void DeleteUser(User user)
